4d794ec80746b9fe9cab10ad2d68f6b5fa847781
fatehkamil <fatehkamilmk@gmail.com>
2026-05-24 17:04:11 +0800

    1.1.0

---
+++ Lab-112.py
@@ -0,0 +1,9 @@
+#Categorizing Values
+#Use numeric data types
+#Use string data types
+#Use the list data type
+
+#Mixed-Type List
+myMixedList = [45, 290578, 1.02, True, "My cat is black", "45"]
+for item in myMixedList:
+    print("{} is of the data type {}".format(item, type(item)))
