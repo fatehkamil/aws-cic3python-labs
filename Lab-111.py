2d1f678b36551e4c6112b50f4471a421cb3e9f15
fatehkamil <fatehkamilmk@gmail.com>
2026-05-24 16:12:09 +0800

    1.1

---
+++ Lab-111.py
@@ -0,0 +1,28 @@
+#Lists, Tuples, and Dictionaries
+#Lists Data Type
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+#List by Position
+print(myFruitList[0]) #First item
+print(myFruitList[1]) #Second item
+print(myFruitList[2]) #Third item
+#Change Value in List
+myFruitList[2] = "orange"
+print(myFruitList)
+#Tuples Data Type
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+#Tuple by Position
+print(myFinalAnswerTuple[0]) #First item
+print(myFinalAnswerTuple[1]) #Second item
+print(myFinalAnswerTuple[2]) #Third item
+#Dictionaries Data Type
+myFavoriteFruitDictionary = {"Pateh": "apple", "Pako": "banana", "Piti": "pineapple"}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+#Dictionary by Name
+print(myFavoriteFruitDictionary["Pateh"])   #Pateh's favorite fruit
+print(myFavoriteFruitDictionary["Pako"])    #Pako's favorite fruit
+print(myFavoriteFruitDictionary["Piti"])    #Piti's favorite fruit
